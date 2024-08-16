479a5108d871147df56aff6bc54927142969191a
Yemyam <ethan.braum@gmail.com>
2024-08-16 12:41:48 +0800

    clean code

--- script.js
+++ script.js
@@ -70,11 +70,6 @@ btns.forEach((btn) => {
         }
         else if(operator == ""){
             console.log(btn.textContent)
-            // add negative symbol
-            // if((btn.textContent == "-") && (op1 == 0)){
-            //     op1 = btn.textContent;
-            // }
-            // Check if only digit is 0, if so replace it
             if((btn.textContent == ".") && !op1.includes(".")){
                 if(op1.length < 16){
                     op1 += btn.textContent;
@@ -105,28 +100,7 @@ btns.forEach((btn) => {
                 op2 += btn.textContent;
                 lowerDisplay.textContent = op2;
             }
-            // when operator is chosen evaluate and add the next operator, or change the current operator
-            // display.textContent = op1 + " " + operator + " " + op2;
         }
-        // else if(operator == ""){
-        //     operator = getOper(btn.textContent,operator,op1)
-        //     console.log(operator)
-        //     op1 = getNum(btn.textContent,op1)
-        //     console.log(op1)
-        //     display.textContent = op1 + " " + operator;
-        // }
-        // else if(operator == "-"){
-        //     // add logic for negative operator !!!
-        //     op2 = getNum(btn.textContent,op2);
-        //     operator = getOper(btn.textContent,operator,op2);
-        //     display.textContent = op1 + " " + operator + " " + op2;
-        // }
-        // else{
-        //     operator = getOper(btn.textContent,operator,op1)
-        //     op2 = getNum(btn.textContent,op2)
-        //     console.log(op2)
-        //     display.textContent = op1 + " " + operator + " " + op2;
-        // }
     })
 })
 
@@ -136,60 +110,3 @@ function isOper(input){
     }
     return false;
 }
-
-// function getNum(value,op){
-//     // add logic for calculating when op1,op2,and operator are valid
-
-//     // remove 0 when first digit is entered
-//     if((!isNaN(value)) && (op == "0")){
-//         op = "";
-//     }
-//     switch(value){
-//         case "0": if (op == "0"){
-//             return op;
-//         }
-//         else{
-//             return op += "0";
-//         }
-//         case "1": return op += "1";
-//         case "2": return op += "2";
-//         case "3": return op += "3";
-//         case "4": return op += "4";
-//         case "5": return op += "5";
-//         case "6": return op += "6";
-//         case "7": return op += "7";
-//         case "8": return op += "8";
-//         case "9": return op += "9";
-//         case "-": 
-//         if(op == ""){
-//             return "-" + op;
-//         }
-//         else{
-//             return op
-//         }
-//     }
-//     return op;
-// }
-
-// function getOper(value,operator,op){
-//     // if op1 and op2 filled do not update
-//     if(op == "-"){
-//         return '';
-//     }
-//     switch(value){
-//         case "+": return "+";
-//         case "-": 
-//         if(op == "-"){
-//             return ""
-//         }
-//         if(op == "0"){
-//             return "-";
-//         }
-//         return "-";
-//         case "x": return "x";
-//         case "÷": return "÷";
-//         }
-//     return operator
-// }
-
-
